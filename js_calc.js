0664a81f054186f877ddff5904b8e378c6ea1c3c
sarahouk91 <159137147+sarahouk91@users.noreply.github.com>
2024-09-05 18:27:53 +0200

    Initial commit

---
+++ js_calc.js
@@ -0,0 +1,43 @@
+let ecran = document.querySelector(".ecran");
+let touches = document.querySelectorAll("button");
+let egal = document.getElementById("egal");
+
+
+let operation ='';
+let resultat='';
+
+//ecran.textContent = `coucou`; 
+console.log("HELLO WORLD");
+
+
+//addEventListener
+//test : 
+/*function test (){
+        prompt("Quel ets chepaquoi");
+}*/
+
+let leftp = document.getElementById("leftp");
+let rightp = document.getElementById("rightp");
+let del = document.getElementById("delete");
+let divide = document.getElementById("divide");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let multiply = document.getElementById("multiply");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let minus = document.getElementById("minus");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let add = document.getElementById("add");
+let point = document.getElementById("point");
+let zero = document.getElementById("zero");
+let egual = document.getElementById("egal");
+
+seven.addEventListener("click", function(){
+        alert("7");
+        //ecran.innerHTML = "7";
+});
+
